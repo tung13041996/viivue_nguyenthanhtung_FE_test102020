7e37191556178a015ed01b3a007ecb839e8520c4
Nguyen Thanh Tung <nguyenthanhtungbk96@gmail.com>
2020-11-01 11:42:02 +0700

    init project ViiVue  FE Test

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const lstShowContentBtn = document.querySelectorAll('.active span');
+
+for (let btn of lstShowContentBtn){
+    btn.addEventListener('click', function(){
+        let textHide = this.parentNode.nextElementSibling;
+        textHide.classList.toggle('fade');
+    });
+}
+
+
+
+let hamBtn = document.querySelector('nav .ham-btn');
+let navMenu = document.getElementById('menu');
+
+hamBtn.addEventListener('click', () =>{
+    navMenu.classList.toggle('show');
+})
+
+var acc = document.getElementsByClassName("contentBox");
+var currentBox = -1;
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    var accActive = document.querySelector(".contentBox.active")
+    if (accActive) {      
+      accActive.classList.remove("active");
+    }
+    if (i !== currentBox) {      
+      this.classList.toggle("active");
+      currentBox = i;
+    } else {
+      currentBox = -1;
+    }
+ });
+}
+
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,    
+    appendDots: $('.dotted'),
+    vertical: true,
+    centerMode:true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    initialSlide: 0,
+    swipe: true,
+    touchMove: true,
+    verticalSwiping: true,
+  });
+  });
